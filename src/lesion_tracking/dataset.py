c2a8effebf38cdbf8f50d5ad08e4aa8ee8a01744
Felipe Coutinho <fcoelho.mrc@gmail.com>
2026-02-04 00:56:33 +0000

    dataset: custom collate

--- src/lesion_tracking/dataset.py
+++ src/lesion_tracking/dataset.py
@@ -2,12 +2,14 @@ import json
 import re
 from pathlib import Path
 from time import perf_counter
-from typing import Any, Mapping
+from typing import Any, Mapping, Optional
 
+import einops
 import torch
 from monai.data import MetaTensor
 from monai.data.image_reader import ImageReader, NibabelReader
 from monai.transforms import (
+    AsDiscreted,
     Compose,
     EnsureChannelFirstd,
     LoadImaged,
@@ -17,6 +19,7 @@ from monai.transforms import (
     Spacingd,
     ToTensord,
 )
+from numpy.typing import NDArray
 from torch.utils.data import DataLoader, Dataset
 
 from lesion_tracking.logger import get_logger
@@ -24,6 +27,7 @@ from lesion_tracking.logger import get_logger
 logger = get_logger(__name__)
 logger.setLevel("DEBUG")
 
+METADATA_PATTERN = re.compile(r"^metadata\.json$")
 CASE_PATTERN = re.compile(r"^case_\d{4}$")  # case_XXXX
 DATA_PATTERN = re.compile(r"^case_\d{4}_t\d$")  # case_XXXX_tn
 
@@ -50,7 +54,7 @@ def sort_dict_of_lists(d: dict[str, list]) -> dict[str, list]:
     return d_sorted
 
 
-def scan_dataset_dir(path: str | Path, require_masks=False) -> list[dict]:
+def scan_dataset_dir(path: str | Path, require_masks=False) -> tuple[list[dict], dict]:
     """
     Scans a dataset directory and returns a list of case entries.
 
@@ -62,9 +66,14 @@ def scan_dataset_dir(path: str | Path, require_masks=False) -> list[dict]:
         "Dataset is missing the metadata.json file"
     )
 
+    metadata = {}
     cases = {}
     for item in path.iterdir():
-        if not item.is_dir() or not CASE_PATTERN.match(item.name):
+        if item.is_file() and METADATA_PATTERN.match(item.name):
+            logger.debug(f"Loading metadata from {item}")
+            metadata = json.load(item.open())
+
+        if item.is_dir() and not CASE_PATTERN.match(item.name):
             continue
 
         scans = sorted(str(p) for p in (item / "scans").glob("*.nii.gz"))
@@ -77,7 +86,10 @@ def scan_dataset_dir(path: str | Path, require_masks=False) -> list[dict]:
 
         cases[item.name] = {"case_id": item.name, "scans": scans, "masks": masks}
 
-    return [cases[k] for k in sorted(cases)]
+    if len(metadata) == 0:
+        logger.warning(f"No metadata was found in {path}")
+
+    return ([cases[k] for k in sorted(cases)], metadata)
 
 
 class LongitudinalDataset(Dataset):
@@ -93,12 +105,23 @@ class LongitudinalDataset(Dataset):
         self._prepare_dataset(dataset_path)
         self.num_cases = len(self.samples)
 
-        self._prepare_loader()  # reads from disk and applies basic preprocessing
+        self._prepare_full_pipeline()
 
     def _prepare_dataset(self, path):
-        self.samples = scan_dataset_dir(path, require_masks=True)
+        self.samples, self.metadata = scan_dataset_dir(path, require_masks=True)
 
     def _prepare_loader(self):
+        """
+        Prepares a MONAI pipeline to perform:
+
+            1. Read image from disk
+            2. Reorient to RAS
+            3. Resample to unit volume
+            4. Normalize with z-score method
+
+        The API handles differences in preprocessing steps between volumes and segmentation masks,
+        while automatically updating the affine matrix.
+        """
         self._loading_pipeline = Compose(
             [
                 LoadImaged(
@@ -121,41 +144,116 @@ class LongitudinalDataset(Dataset):
                     keys=["scans"],
                     channel_wise=True,
                 ),
-                # TODO: move this elsewhere
-                ToTensord(keys=["scans", "masks"]),
             ],
         )
+
+    def _prepare_augmentations(self): ...
+
+    def _prepare_full_pipeline(self):
+        self._prepare_loader()
+        self._prepare_augmentations()
+        self._full_pipeline = Compose(
+            [
+                self._loading_pipeline,
+                ToTensord(keys=["scans", "masks"]),
+                AsDiscreted(keys=["masks"]),
+                # self._augmentation_pipeline,
+            ],
+        ).flatten()
+
+    def _get_metadata(self, idx) -> Optional[Any]:
+        case_id = self.samples[idx].get("case_id")
+        if case_id:
+            return self.metadata.get(case_id)
 
     def __len__(self):
         return self.num_cases
 
     def __getitem__(self, idx):
+        """
+        Output:
+            dict with keys
+            scans -> list of L tensors with shape (C, H, W, D)
+            masks -> list of L tensors with shape (C, H, W, D)
+            case_id -> str with patient identifier
+        """
         sample = self.samples[idx]
         num_timepoints = len(sample["scans"])
         logger.debug(
             f"Retrieving sample {idx} ({sample['case_id']}) -> Found {num_timepoints} timepoints"
         )
 
-        output = {"case_id": sample["case_id"], "scans": [], "masks": []}
+        output = {
+            "case_id": sample["case_id"],
+            "metadata": self._get_metadata(idx),
+            "scans": [],
+            "masks": [],
+        }
         for i in range(num_timepoints):
             tmp = {"scans": sample["scans"][i], "masks": sample["masks"][i]}
-            tmp_out = self._loading_pipeline(tmp)
+            tmp_out = self._full_pipeline(tmp)
             assert isinstance(tmp_out, dict), (
                 f"Unexpected type after loading pipeline: {type(tmp_out)}"
             )
-            self._custom_merge(output, tmp_out)
+            self._fill_lists_from_dict(output, tmp_out)
 
         return output
 
     @staticmethod
-    def _custom_merge(d1: Mapping, d2: Mapping):
+    def _fill_lists_from_dict(d1: Mapping, d2: Mapping):
         """
         d1: Mapping where some keys point to lists
-        d2: Fill d1 lists with values from matching keys
+        d2: Fill d1 lists with d2 values from matching keys
         """
         for k2, v2 in d2.items():
             if k2 in d1 and isinstance(d1[k2], list):
                 d1[k2].append(v2)
+
+
+def longitudinal_collate_fn(batch):
+    """Custom collate that handles variable-length sequences."""
+    return {
+        "case_id": [item["case_id"] for item in batch],
+        "metadata": [item.get("metadata") for item in batch],
+        "scans": [item["scans"] for item in batch],  # list of lists
+        "masks": [item["masks"] for item in batch],
+    }
+
+
+def iterate_over_batch(batch):
+    batched_case_ids = batch.get("case_id")
+    batched_scans = batch.get("scans")
+    batched_masks = batch.get("masks")
+    for case_id, scans, masks in zip(batched_case_ids, batched_scans, batched_masks):
+        yield case_id, scans, masks
+
+
+def iterate_over_timepoints(batch):
+    for case_id, scans, masks in iterate_over_batch(batch):
+        assert len(scans) == len(masks), (
+            f"Found mismatched {len(scans)} scans and {len(masks)} masks during iteration"
+        )
+        for scan, mask in zip(scans, masks):
+            yield case_id, scan, mask
+
+
+def without_channel(x: torch.Tensor) -> torch.Tensor:
+    assert x.ndim == 4
+    assert x.shape[0] == 1  # single-channel image
+    return einops.rearrange(x, "1 h w d -> h w d")
+
+
+# TODO: add splits, custom collate, cache dataset? persistent dataset? custom sampling strategies?
+def get_loader(dataset_path, batch_size=1, shuffle=False, num_workers=1):
+    dataset = LongitudinalDataset(dataset_path=dataset_path)
+    loader = DataLoader(
+        dataset=dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        num_workers=num_workers,
+        collate_fn=longitudinal_collate_fn,
+    )
+    return loader
 
 
 if __name__ == "__main__":
